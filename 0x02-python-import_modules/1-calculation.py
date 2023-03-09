f8222915c492f1f7a253d7eeb52745c27efb9cba
macobilor <sixtus4real2k2@ymail.com>
2023-03-09 10:03:40 -0800

    task

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,13 @@
+#!/urs/bin/python3
+
+if_name_ == " _main_":
+    """Print the sum, difference, multiple and quotient of
+    from calculate_1 import add, sub, mul, div
+
+    a = 10
+    b = 5
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}.format(a, b, sub(a, b)))
+    print("{} * {} = {}.format(a, b, mul(a, b)))
+    print("{} / {} = {}.format(a, b, div(a, b)))
